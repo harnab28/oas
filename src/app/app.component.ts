73df3e27f5db95ed54d7b0f7fec08c58630e462e
Arnab Hazra <harnab28@gmail.com>
2020-09-05 21:00:56 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import { TestserviceService } from './services/testservice.service';
+import { question, answer } from './question.interface'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
 export class AppComponent {
+
   title = 'OASys';
+  questionBank: question[];
+  questionAnswer: answer;
+
+  answerList : any = [];
+  
+
+  test : string;
+
+  constructor(private ts : TestserviceService){
+    this.questionBank = ts.getQuestion();
+    console.log(this.questionBank)
+    this.answerList = this.questionBank.map(questionData => ({
+      questionId: questionData.questionId,
+      answer : null
+    }))
+
+
+    
+  }
+
+
+  check(){
+    console.log(this.answerList);
+  }
+
+  createans(){
+
+  }
+
+
 }
---
+++ src/app/services/testservice.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { QuestionBank } from '../question.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TestserviceService {
+  
+  question : any;
+
+  
+  constructor() { 
+    this.question = QuestionBank;
+  }
+
+  getQuestion(){
+    return this.question;
+  }
+
+
+
+
+}
