3fd1300687f448e73b64c4e64072495418c42d78
Arnab Hazra <harnab28@gmail.com>
2020-09-09 01:24:55 +0530

    Two Component Created under Component Folder --> quiz, timer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './components/test/test.component';
+import { TimerComponent } from './components/timer/timer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    TimerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/timer/timer.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'timer',
+  templateUrl: './timer.component.html',
+  styleUrls: ['./timer.component.scss']
+})
+export class TimerComponent implements OnInit {
+
+  @Input() endSeconds: number;
+  @Output() finished  = new EventEmitter<boolean> ();
+
+  constructor() { 
+    
+  }
+
+  ngOnInit(): void {
+    //console.log(this.endSeconds)
+    this.startTimer();
+  }
+
+  //timer realted variables
+  timer: any = null;
+  startTime: Date;
+  endTime: Date;
+  ellapsedTime = '00:00';
+  duration = '';
+
+  startTimer() {
+    this.startTime = new Date();
+    this.ellapsedTime = '00:00';
+    this.timer = setInterval(() => {
+      this.tick();
+    }, 1000);
+    this.duration = this.parseTime(this.endSeconds);
+  }
+
+  parseTime(totalSeconds: number) {
+    let mins: string | number = Math.floor(totalSeconds / 60);
+    let secs: string | number = Math.round(totalSeconds % 60);
+    mins = (mins < 10 ? '0' : '') + mins;
+    secs = (secs < 10 ? '0' : '') + secs;
+    return `${mins}:${secs}`;
+  }
+
+  tick() {
+    const now = new Date();
+    const diff = (now.getTime() - this.startTime.getTime()) / 1000;
+    if (diff >= this.endSeconds) {
+      this.finished.emit(true);
+    }
+    else
+    {
+      this.finished.emit(false);
+    }
+    this.ellapsedTime = this.parseTime(diff);
+  }
+
+}
